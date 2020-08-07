760edb2ab0e59697fe2e8f560e85e14bdfb8e7c0
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-08-07 23:06:18 +0900

    created form

--- app/javascript/bundles/User/components/CreateComponent.jsx
+++ app/javascript/bundles/User/components/CreateComponent.jsx
@@ -1,9 +1,28 @@
 import React, {Component} from 'react'
 
 class CreateComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: ''
+    }
+  }
+
+  onChangetxet(e) {
+    this.setState({product: e.target.value})
+  }
   render() {
     return (
-      <div>Hi this is the create form</div>
+      <div>
+        <p>Hi this is the create form</p><br />
+        <form>
+          <p>name</p>
+          <input />
+          <p>post</p>
+          <textarea/>
+          <input type='submit' value='作成'/>
+        </form>
+      </div>
     );
   };
 };

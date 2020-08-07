08f455938a2481f10398c001660060e055a36f79
xfpng345 <54070896+xfpng345@users.noreply.github.com>
2020-08-07 17:10:38 +0900

    不要な処理、ページを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,11 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:update, :destroy]
 
   # GET /users
   # GET /users.json
   def index
     @users = User.all
     @usersj = @users.as_json
-  end
-
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
   end
 
   # POST /users
